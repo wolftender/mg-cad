464f0790c6832174f636f388d3d22f250c2824b5
wolftender <etheryt@protonmail.com>
2023-08-19 19:17:21 +0200

    improve initial point finding algorithm

--- src/intersection.cpp
+++ src/intersection.cpp
@@ -20,14 +20,20 @@ namespace mini {
 		}
 
 		glm::vec2 p1, p2;
-		m_find_starting_points(p1, p2);
+		glm::vec2 s1 = { 0.5f, 0.5f };
+		glm::vec2 s2 = { 0.5f, 0.5f };
+
+		if (!m_find_starting_points(p1, p2, s1, s2)) {
+			std::cout << "no intersection found between surfaces" << std::endl;
+			return;
+		}
 	}
 
 	intersection_controller::~intersection_controller() { }
 
-	void intersection_controller::m_find_starting_points(const glm::vec2 & p1, const glm::vec2 & p2) const {
+	bool intersection_controller::m_find_starting_points(glm::vec2 & p1, glm::vec2 & p2, const glm::vec2 & s1, const glm::vec2 & s2) const {
 		if (!m_surface1 || !m_surface2) {
-			return;
+			return false;
 		}
 
 		// surface1 is parameterized by u,v
@@ -62,12 +68,14 @@ namespace mini {
 		};
 		
 
-		glm::vec4 current = { 0.5f, 0.5f, 0.5f, 0.5f };
+		glm::vec4 current = { s1.x, s1.y, s2.x, s2.y };
 		glm::vec4 previous = current;
 
 		float step = 0.001f;
 		float epsilon = 0.00001f;
 		float d1 = 0.0f, d2 = 0.0f;
+
+		int num_steps = 0, epsd = 0;
 
 		do {
 			glm::vec4 direction = {
@@ -90,9 +98,29 @@ namespace mini {
 			d1 = glm::sqrt(du*du + dv*dv);
 			d2 = glm::sqrt(dp*dp + dq*dq);
 
-			std::cout << "step " << direction.x << ", " << direction.y << ", " << direction.z << ", " << direction.w << std::endl;
-			std::cout << "pos " << current.x << ", " << current.y << ", " << current.z << ", " << current.w << std::endl;
-		} while (d1 > epsilon || d2 > epsilon);
+			num_steps++;
+			if (d1 < epsilon && d2 < epsilon && epsd < 3) {
+				epsd++;
+				epsilon = epsilon / 5.0f;
+				step = step / 5.0f;
+			}
+
+			//std::cout << "step " << direction.x << ", " << direction.y << ", " << direction.z << ", " << direction.w << std::endl;
+			//std::cout << "pos " << current.x << ", " << current.y << ", " << current.z << ", " << current.w << std::endl;
+		} while (d1 > epsilon || d2 > epsilon && num_steps < 500);
+
+		// check if this is actually a point of intersection
+		auto pos1 = m_surface1->sample(current[0], current[1]);
+		auto pos2 = m_surface2->sample(current[2], current[3]);
+
+		auto dist = glm::distance(pos1, pos2);
+
+		if (dist > 0.01f) {
+			return false;
+		}
+
+		p1 = { current[0], current[1] };
+		p2 = { current[2], current[3] };
 
 		const auto point_obj1 = std::make_shared<point_object>(m_scene,
 			m_store->get_billboard_s_shader(),
@@ -102,13 +130,15 @@ namespace mini {
 			m_store->get_billboard_s_shader(),
 			m_store->get_point_texture());
 
-		point_obj1->set_translation(m_surface1->sample(current[0], current[1]));
-		point_obj2->set_translation(m_surface2->sample(current[2], current[3]));
+		point_obj1->set_translation(pos1);
+		point_obj2->set_translation(pos2);
 
 		point_obj1->set_color({ 1.0f, 0.0f, 0.0f, 1.0f });
-		point_obj2->set_color({1.0f, 0.0f, 0.0f, 1.0f});
+		point_obj2->set_color({ 1.0f, 0.0f, 0.0f, 1.0f });
 
 		m_scene.add_object("debug_point", point_obj1);
 		m_scene.add_object("debug_point", point_obj2);
+
+		return true;
 	}
 }
